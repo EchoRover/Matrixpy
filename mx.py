6b531a00d0c6d136d89ec49d914b16679e09149b
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2024-01-09 23:07:00 +0400

    Matrix module finished

--- mx.py
+++ mx.py
@@ -8,9 +8,10 @@ class Matrix:
             self.m = self.n = size
             self.createMatrix(data.lower())
         else:
-            self.m = len(data[0])
-            self.n = len(data)
+            self.m = len(data) #rows no
+            self.n = len(data[0])# cols no
             self.data = data
+        self.details = f"{self.m} x {self.n}"
 
     def createMatrix(self, types):
         if types == 'o':
@@ -34,10 +35,12 @@ class Matrix:
             raise type(other) + "cant be mutiplyed with matrix"
 
         if self.n != other.m:
-            raise f"cant multiple ({self.m},{self,n}) and ({other.m,other.n})"
+            raise f"cant multiple ({self.m},{self.n}) and ({other.m,other.n})"
+  
 
         othert = other.t().data
-        self.new = [[0 for x in range(self.m)] for y in range(self.n)]
+        self.new = [[0 for x in range(other.n)] for y in range(self.m)]
+
         for y, myrow in enumerate(self.data):
             for x, otherrow in enumerate(othert):
                 self.new[y][x] = sum(map(lambda x, y: x * y, myrow, otherrow))
@@ -51,21 +54,28 @@ class Matrix:
             return Matrix(self.new)
 
     def __add__(self, other):
+
         if not type(other) == Matrix:
             return
         if other.m != self.m or other.n != self.n:
             return
 
-        self.new = [[self.data[y][x] * other.data[y][x]
+        self.new = [[self.data[y][x] + other.data[y][x]
                      for x in range(self.m)] for y in range(self.n)]
 
         return Matrix(self.new)
+    def __sub__(self,other):
+
+        return self.__add__(other.__mul__(-1))
+
 
     def __eq__(self, other):
+        if type(other) != Matrix:
+            return False
         if self.m != other.m or self.n != other.n:
             return False
-        for j in range(self.n):
-            for i in range(self.m):
+        for j in range(self.m):
+            for i in range(self.n):
                 if round(self.data[j][i], 2) != round(other.data[j][i], 2):
                     # print(self.data[j][i] , other.data[j][i],other)
                     return False
@@ -76,8 +86,7 @@ class Matrix:
 
     def t(self):
         """ returns transpose of matrix """
-        if self.n != self.m:
-            return
+       
         return Matrix(list(map(list, zip(*self.data))))
 
     def inv(self):
@@ -159,34 +168,54 @@ def randmat(size=3):
     """ generates random square matrix of give size """
     return [[randint(-3, 4) for _ in range(size)] for __ in range(size)]
 
-def help():
+def mxhelp():
     print("Matrix class")
     print("""
-    data / I / O
-    add + 
-    multiply *
-    inv
-    copy
+    1)creating
+
+    a = Matrix([[1,0],[2,-3]])
+    b = Matrix("i",2) #can also be O 
+
+    2)operations
+
+    a + b
+    a - b
+    a * b
+    c = a.inv() #returns inverse matrix if exists else None
+    c = a.copy()
     """)
     print("Other funcs""")
     print("""
-    adj(matrix)
-    det(matrix)
+    c = adj(matrix) # returns adjoint of matrix
+    d = det(matrix) # return determinant
     randmat(size) #genrates random square matrix value C [-3.4]
     """)
 
 if __name__ == "__main__":
+
+   
+    assert (Matrix([[1,2,-3],[3,2,-2],[2,-1,1]]).inv() * Matrix([[6],[3],[2]]))== Matrix([[1],[-5],[-5]]) ,"odd mult wrong"
+    
+
+
+    assert  Matrix([[1,2],[3,4],[5,6]]) * Matrix([[1,2],[3,4]]) ,"multiplication error"
+  
+
+
+    assert Matrix("o",2) - Matrix("i",2) == Matrix("i",2) * -1 ,"subtractions wrong"
+
 
     # know case
     sample = Matrix([[1, 2, 3], [4, 2, 1], [7, 8, 9]])
     assert sample * sample.inv() == Matrix("I", 3), "something is wrong"
     # 2x2 random matrix
     sample = Matrix(randmat(2))
-    assert sample * sample.inv() == Matrix("I", 2) or sample.inv() == None, "something is wrong"
+    
+    assert sample.inv() == None or sample * sample.inv() == Matrix("I", 2) , "something is wrong"
     # 3x3 random matrix
     sample = Matrix(randmat(3))
-    assert sample * sample.inv() == Matrix("I", 3) or sample.inv() == None, "something is wrong"
+    assert sample.inv() == None or sample * sample.inv() == Matrix("I", 3) , "something is wrong"
 
     print("ALL TESTS SUCCESS")
 
-    help()
+    mxhelp()
